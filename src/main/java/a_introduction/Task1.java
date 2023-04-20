256e27da8b66590da7daba7df48f970aeb8faefa
piotr.ziolkowski <piotr.ziolkowski1990@interia.eu>
2023-04-20 17:53:22 +0200

    po pierwszym cwiczeniu

--- src/main/java/a_introduction/Task1.java
+++ src/main/java/a_introduction/Task1.java
@@ -2,5 +2,12 @@ package a_introduction;
 
 public class Task1 {
     public static void main(String[] args) {
+    int a=16;
+    char b='B';
+    double c=1.25;
+    long d= 10456;
+    boolean e=true;
+
     }
+
 }
--- src/main/java/a_introduction/Task2.java
+++ src/main/java/a_introduction/Task2.java
@@ -2,5 +2,7 @@ package a_introduction;
 
 public class Task2 {
     public static void main(String[] args) {
+        int magic=2147483647;
+        System.out.println(magic+1);
     }
 }
